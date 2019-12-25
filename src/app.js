70a95755ac18c592f7d16fc705ee2fe0be0b06c6
Brayden Copley <braydenc303@gmail.com>
2019-12-25 13:55:41 -0700

    boilerplate front end app.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
   
-ReactDOM.render(<IndecisionApp/>, document.getElementById("app"));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
